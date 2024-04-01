0bf034dcaf365e8bdcfed33acc62444ae8192f0b
unknown <you@example.com>
2024-04-01 13:31:45 +0200

    tch

---
+++ build_windows.bat
@@ -0,0 +1,1 @@
+cmake . -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=toolchain\Windows.toolchain.cmake
---
+++ toolchain/Windows.toolchain.cmake
@@ -0,0 +1,6 @@
+
+set(CMAKE_SYSTEM_NAME "Windows")
+set(CMAKE_BUILD_TYPE "Debug")
+set(CMAKE_C_COMPILER "C:\\w64devkit\\bin\\gcc.exe")
+set(CMAKE_CXX_COMPILER  "C:\\w64devkit\\bin\\g++.exe")
+set(CMAKE_CXX_STANDARD 17)
